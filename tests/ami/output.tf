2810494be91639ee0f68a30e28f60526afeca276
kiranm <kiran01bm@gmail.com>
2018-12-17 08:12:16 +1100

    adding AMI module

---
+++ tests/ami/output.tf
@@ -0,0 +1,9 @@
+output "base_image_root_device_name" {
+  value     = "${local.base_image_root_device_name}"
+}
+
+output "base_image_id" {
+  value     = "${local.base_image_id}"
+}
+
+
---
+++ tests/ami/variables.tf
@@ -0,0 +1,8 @@
+variable "platform_name" {
+  default = "test"
+}
+
+variable "use_community" {
+  default = false
+}
+
