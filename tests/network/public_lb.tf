6bbe6e3806c744d03b0d1dd78c2582457965319a
kiranm <kiran01bm@gmail.com>
2018-12-16 19:36:09 +1100

    Adding Public LB, Public SG and associated Listeners and Target Groups

---
+++ tests/network/public_lb.tf
@@ -0,0 +1,18 @@
+resource "aws_lb" "public" {
+  name                             = "${var.platform_name}-public"
+  internal                         = false
+  load_balancer_type               = "network"
+  subnets                          = ["${aws_subnet.public.*.id}"]
+
+  # This is to enable cross zone load balancing across the AZs
+  enable_cross_zone_load_balancing = true
+
+  tags = "${map(
+    "kubernetes.io/cluster/${var.platform_name}", "owned",
+    "Name", "${var.platform_name}-public"
+  )}"
+}
+
+data "dns_a_record_set" "platform_public_ip_set" {
+  host = "${aws_lb.public.dns_name}"
+}
